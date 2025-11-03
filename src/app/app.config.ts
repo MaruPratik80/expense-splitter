a1600f7a1d3f30cf494786e3c3907ea5ed55d391
MaruPratik80 <pratikmaru80@gmail.com>
2025-11-03 20:24:27 +0530

    Added Angular Material, Tailwind, and Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,45 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'expense-splitter-project',
+        appId: '1:1015183181455:web:3ab752bd0dd64542e552fa',
+        storageBucket: 'expense-splitter-project.firebasestorage.app',
+        apiKey: 'AIzaSyDbJzHopUnJAfet6dqfJjQRgSzJF-hSkY0',
+        authDomain: 'expense-splitter-project.firebaseapp.com',
+        messagingSenderId: '1015183181455',
+        measurementId: 'G-M2PPETMNPC',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage()),
+  ],
 };
